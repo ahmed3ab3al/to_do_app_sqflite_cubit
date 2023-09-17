e34538888000d1ccc6571e7a30473763fc847a8f
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2023-09-17 22:40:09 +0300

    first commit

---
+++ lib/shared/cubit/states.dart
@@ -0,0 +1,21 @@
+abstract class AppStates{}
+
+class AppInitialState extends AppStates{}
+
+class AppChangeBottomNavBarState extends AppStates{}
+
+class AppCreateDataBaseState extends AppStates{}
+
+class AppGetDataBaseState extends AppStates{}
+
+class AppUpDateDataBaseState extends AppStates{}
+class AppDeleteDataBaseState extends AppStates{}
+
+class AppGetDataBaseLoadingState extends AppStates{}
+
+class AppInsertDataBaseState extends AppStates{}
+
+class AppChangeButtomSheetState extends AppStates{}
+
+
+
